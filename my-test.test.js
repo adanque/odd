a9c4a301bd426988e6d2516755a56e994375aa10
Alan Danque <adanque@gmail.com>
2025-09-29 18:20:05 -0500

    files updated

--- my-test.test.js
+++
@@ -1,9 +0,0 @@
-describe('Math operations', () => {
-  test('adds 1 + 1 to equal 2', () => {
-    expect(1 + 1).toBe(2);
-  });
-
-  test('subtracts 5 - 2 to equal 3', () => {
-    expect(5 - 2).toBe(3);
-  });
-});
